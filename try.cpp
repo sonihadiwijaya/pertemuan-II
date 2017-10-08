ab02988051231098da7044e16a05425aa73d8c5c
sonihadiwijaya <hadiwijayasoni@gmail.com>
2017-10-08 18:22:45 +0700

    versi 2.14.2

---
+++ try.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "algoritma memasak nasi" <<endl;
+cout << "1 .ambil beras dan air" << endl;
+cout << "2 .ambil wadah" << endl;
+cout << "3 .masukan beras dan air" << endl;
+cout << "4 .cucilah beras sampai bersih" << endl;
+cout << "5 .buanglah air cucian beras tersebut" << endl;
+cout << "6 .lalu masukan beras yg sudah dicuci ke dalam rice cooker" <<endl;
+cout << "7 .masukan air bersih secukupnya ke dalam rice cooker" <<endl;
+cout << "8 .lalu tutup rice cooker dan nyalakan rice cooker" <<endl;
+cout << "9 .tunggu sampai matang" << endl;
+cout << "10.setelah matang , ambil wadah atau piring" << endl;
+cout << "11.pindahkan nasi ke dalam piring" << endl;
+cout << "12.nasi siap disantap" <<endl;
+
+return 0;
+}
